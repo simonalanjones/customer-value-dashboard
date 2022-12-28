970226371533f0d57e95be1fda037fea1150ddcc
Simon Jones <simonajones@gmail.com>
2022-12-28 12:31:16 +0000

    Page header text updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <>
       <h1 className="text-5xl text-gray-700 font-bold mb-5 ">
-        Customer Value Tool
+        Customer Value Dashboard
       </h1>
       <p className="text-gray-500 text-lg">
         For individual core plan member use only, this does not include partner

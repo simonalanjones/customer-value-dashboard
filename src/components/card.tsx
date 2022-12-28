a666c47c98a3b65bebbd459efc21ccb965604b21
Simon Jones <simonajones@gmail.com>
2022-12-28 12:45:02 +0000

    Updated card styles

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,12 @@
 import { CardProps, RatingCardProps, CurrencyCardProps } from '../interfaces';
 import IconRating from './iconRating';
 
-// Tailwind style for the outer card
-const cardOuter: string =
-  'h-36 border border-gray-200 px-6 py-6 bg-gray-100/50';
-// Tailwind style for the card title
-const cardTitle: string =
-  'mb-2 text-gray-500 font-bold text-sm uppercase tracking-wider';
-// Tailwind style for the card description
-const cardDescription: string = 'text-gray-700 font-bold text-xl';
-
+// see index.css for tailwind styles
 const Card = ({ title, description }: CardProps) => {
   return (
-    <div className={cardOuter}>
-      <h2 className={cardTitle}>{title}</h2>
-      <p className={cardDescription}>{description}</p>
+    <div className="card-outer">
+      <h2 className="card-title">{title}</h2>
+      <p className="card-description">{description}</p>
     </div>
   );
 };
@@ -26,9 +18,9 @@ const CurrencyCard = ({ title, amount }: CurrencyCardProps) => {
   });
 
   return (
-    <div className={cardOuter}>
-      <h2 className={cardTitle}>{title}</h2>
-      <p className={cardDescription}>{UKpounds.format(amount)}</p>
+    <div className="card-outer">
+      <h2 className="card-title">{title}</h2>
+      <p className="card-description">{UKpounds.format(amount)}</p>
     </div>
   );
 };
@@ -36,13 +28,13 @@ const CurrencyCard = ({ title, amount }: CurrencyCardProps) => {
 const RatingCard = ({ rating }: RatingCardProps) => {
   return (
     <>
-      <div className={cardOuter}>
+      <div className="card-outer">
         <div className="float-right">
           <IconRating rating={rating} />
         </div>
 
-        <h2 className={cardTitle}>Member rating</h2>
-        <p className={cardDescription}>{rating}</p>
+        <h2 className="card-title">Member rating</h2>
+        <p className="card-description">{rating}</p>
       </div>
     </>
   );
